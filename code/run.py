009df8024f1c89c406522884246013e8d7af254b
Christina Ruddle <ruddlec@tcd.ie>
2019-06-26 15:11:35 -0400

    Changing app to application

--- code/run.py
+++ code/run.py
@@ -1,14 +1,8 @@
-from app import app
+from app import app as application
 from db import db
 
 db.init_app(app)
 
-# SQLAlchemy will create the data.db file automatically, but
-# needs to be told what tables and schemas to create in it
-# Using a Flask decorator (not from an extension)
-
 @app.before_first_request
 def create_tables():
-    # Creating tables in data.db (referenced above), if
-    # they do not already exist
     db.create_all()
